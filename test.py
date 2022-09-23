67405e51470a3dffaa9ec430d1be1b975f81bf6f
Björn Bulkens <b.s.bulkens@gmail.com>
2022-09-23 16:48:22 +0200

    cleaning

--- test.py
+++
@@ -1,9 +0,0 @@
-import torch
-torch.__version__ # Get PyTorch and CUDA version
-torch.cuda.is_available() # Check that CUDA works
-torch.cuda.device_count() # Check how many CUDA capable devices you have
-
-# Print device human readable names
-print(torch.cuda.get_device_name(0))
-#torch.cuda.get_device_name(1)
-# Add more lines with +1 like get_device_name(3), get_device_name(4) if you have more devices.
